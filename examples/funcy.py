97884aaa504bea06a11743310bf1532876346e4f
Adam Ashenfelter <ashenfad@gmail.com>
2025-07-01 21:28:51 -0700

    feat: shorten fn example

--- examples/funcy.py
+++ examples/funcy.py
@@ -10,11 +10,7 @@ from typing import Callable
 
 from agex import Agent, Versioned
 
-funcy_agent = Agent(
-    primer="You are great a building functions. You are given a task and your goal is to build a function that will solve the task.",
-    max_iterations=5,
-)
-
+funcy_agent = Agent(primer="You are great at providing custom functions to the user.")
 funcy_agent.module(math, visibility="low")
 
 
@@ -30,16 +26,14 @@ def example():
     # Use versioned state to maintain context between agent calls
     state = Versioned()
 
-    # First request: build a function to find next prime
-    fn = fn_builder(
-        "Make a fn that finds a prime larger than a given number.", state=state
-    )
+    # build a function to find next prime
+    fn = fn_builder("a fn for the first prime larger than a given number.", state=state)
 
     # the function is callable in native python
     print(fn(500000))
     # 500009
 
-    # Second request: agent remembers context and builds related function
+    # agent remembers existing context and builds related function
     fn = fn_builder("Okay, now make it the next lower prime.", state=state)
 
     print(fn(500000))
